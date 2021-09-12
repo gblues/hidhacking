9a8ae7cf7af6c7025a5684116edb179da31aaeec
Nathan Strong <nstrong@tripwire.com>
2021-09-11 23:02:59 -0700

    Successfully read controller string with HID i/o
    == DETAILS
    I've confirmed this works with a switch pro controller.
    
    It does *not* presently work with a dualsense controller, but
    that's because the ds3 has a proprietary activation packet that
    needs to be sent that I haven't implemented here.
    
    The bytes returned are technically unicode and I'm doing a
    poor man's ASCII conversion here.

--- src/main.c
+++ src/main.c
@@ -6,145 +6,103 @@
 #include <whb/log.h>
 #include <whb/log_console.h>
 #include <nsysuhs/uhs.h>
+#include <nsyshid/hid.h>
 #include <vpad/input.h>
 #include <fcntl.h>
+#include <stdio.h>
 
 #include "hidhacking.h"
 
-UHSStatus UhsGetDescriptorString(UhsHandle *handle, uint32_t if_handle, uint8_t string_index, uint8_t unknwn, void *out_buffer, size_t out_buffer_size);
+// UHSStatus UhsGetDescriptorString(UhsHandle *handle, uint32_t if_handle, uint8_t string_index, uint8_t unknwn, void *out_buffer, size_t out_buffer_size);
 
-void init_uhs_config(UhsConfig *config);
-void teardown_uhs_config(UhsConfig *config);
-void uhs_acquire_callback(void *context, int32_t p1, int32_t p2);
+int32_t hid_attach_callback(HIDClient *client, HIDDevice *device, HIDAttachEvent attach);
 
-void print_string_descriptors(UhsHandle * handle, UhsInterfaceProfile *profile);
-
-typedef struct _usb_context usb_context;
+#define HOST_TO_DEVICE 0x00
+#define DEVICE_TO_HOST 0x80
+#define TYPE_STANDARD  0x00
+#define TYPE_CLASS     0x20
+#define TYPE_VENDOR    0x40
+#define TYPE_RESERVED  0x60
+#define TO_DEVICE      0x00
+#define TO_INTERFACE   0x01
+#define TO_ENDPOINT    0x02
+#define TO_OTHER       0x03
 
 int main(int argc, char **argv)
 {
-   int result;
-   
-   UhsHandle *handle = alloc_cachealigned_zeroed(sizeof(UhsHandle), NULL);
-   UhsConfig *config = alloc_cachealigned_zeroed(sizeof(UhsConfig), NULL);
-   init_uhs_config(config);
-
    WHBProcInit();
    WHBLogConsoleInit();
    WHBLogPrintf("HID Hacking!");
    WHBLogConsoleDraw();
-
-   result = UhsClientOpen(handle, config);
-   switch(result) {
-      case UHS_STATUS_OK:
-         WHBLogPrintf("Successfully opened UHS client!\n");
-         break;
-      case UHS_STATUS_HANDLE_INVALID_ARGS:
-         WHBLogPrintf("UhsClientOpen: invalid arguments\n");
-         goto error;
-         break;
-      case UHS_STATUS_HANDLE_INVALID_STATE:
-         WHBLogPrintf("UhsClientOpen: invalid state\n");
-         goto error;
-         break;
-   }
-   WHBLogConsoleDraw();
-
-   UhsInterfaceProfile profiles[10];
-   UhsInterfaceFilter filter = {
-      .match_params = MATCH_ANY
-   };
-
-   result = UhsQueryInterfaces(handle, &filter, profiles, 10);
-   switch(result) {
-      case UHS_STATUS_OK:
-         WHBLogPrintf("Successfully queried USB interface\n");
-         break;
-      case UHS_STATUS_HANDLE_INVALID_ARGS:
-         WHBLogPrintf("UhsQueryInterfaces: invalid arguments\n");
-         goto error;
-         break;
-      case UHS_STATUS_HANDLE_INVALID_STATE:
-         WHBLogPrintf("UhsQueryInterfaces: invalid state\n");
-         goto error;
-         break;
-      default:
-         if(result < 0) {
-            WHBLogPrintf("UhsQueryInterfaces: got unexpected UhsResult 0x%08x\n", result);
-            goto error;
-         }
-         WHBLogPrintf("Query found %d USB devices\n", result);
-         break;
-   }
-   WHBLogConsoleDraw();
-   int success = 0;
-   for(int i = 0;i<result;i++) {
-       result = UhsAcquireInterface(handle, profiles[i].if_handle, 0, 0);
-
-       switch (result) {
-           case UHS_STATUS_OK:
-               WHBLogPrintf("Successfully aquired device interface: vid: 0x%04X pid: 0x%04X\n", profiles[i].dev_desc.idVendor, profiles[i].dev_desc.idProduct);
-
-               print_string_descriptors(handle, &profiles[i]);
-               UhsReleaseInterface(handle, profiles[i].if_handle, true);
-               break;
-           case UHS_STATUS_HANDLE_INVALID_ARGS:
-               WHBLogPrintf("UhsAcquireInterface: invalid arguments\n");
-               break;
-           case UHS_STATUS_HANDLE_INVALID_STATE:
-               WHBLogPrintf("UhsAcquireInterface: invalid state\n");
-               break;
-       }
-   }
-   error:
-   WHBLogConsoleDraw();
+   HIDClient hid = {0};
+   HIDAddClient(&hid, hid_attach_callback);
 
    while(WHBProcIsRunning()) {
       OSSleepTicks(OSMillisecondsToTicks(100));
    }
 
-
    WHBLogPrintf("Exiting... good bye.");
    WHBLogConsoleDraw();
 
-   if(handle->state > UHS_HANDLE_STATE_INIT) {
-      UhsClientClose(handle);
-   }
-
-   teardown_uhs_config(config);
-   free(handle);
-   free(config);
+   HIDDelClient(&hid);
    OSSleepTicks(OSMillisecondsToTicks(1000));
-
    WHBLogConsoleFree();
    WHBProcShutdown();
    return 0;
 }
 
-void init_uhs_config(UhsConfig *config) {
-   config->controller_num = 0;
-   void *buffer = NULL;
-   uint32_t size = 0;
+void read_descriptor_string(HIDDevice *device);
 
-   init_cachealigned_buffer(5120, &buffer, &size);
-   config->buffer = buffer;
-   config->buffer_size = size;
-}
+int32_t hid_attach_callback(HIDClient *client, HIDDevice *device, HIDAttachEvent attach) {
+   int32_t want_disconnect_event = 0;
 
-void teardown_uhs_config(UhsConfig *config) {
-   if(config->buffer) {
-      free(config->buffer);
-      config->buffer = NULL;
-      config->buffer_size = 0;
+
+   WHBLogPrintf("HID device %s", attach ? "connected" : "disconnected\n");
+   if(attach) {
+      WHBLogPrintf("handle            : 0x%08x\n", device->handle);
+      WHBLogPrintf("physicalDeviceInst: 0x%08x\n", device->physicalDeviceInst);
+      WHBLogPrintf("vendor_id         : 0x%04x\n", device->vid);
+      WHBLogPrintf("product_id        : 0x%04x\n", device->pid);
+      WHBLogPrintf("interfaceIndex    : %d\n", device->interfaceIndex);
+      WHBLogPrintf("subClass          : %d\n", device->subClass);
+      WHBLogPrintf("protocol          : %d\n", device->protocol);
+      WHBLogPrintf("rx_packet_size    : %d\n", device->maxPacketSizeRx);
+      WHBLogPrintf("tx_packet_size    : %d\n", device->maxPacketSizeTx);
+      WHBLogConsoleDraw();
+      read_descriptor_string(device);
+      want_disconnect_event = 1;
    }
+   WHBLogConsoleDraw();
+
+   return want_disconnect_event;
 }
 
-void print_string_descriptors(UhsHandle * handle, UhsInterfaceProfile *profile) {
-    char buffer[512];
-    for(int string_index = 0; string_index<4;string_index++){
-        memset(buffer, 0, 512);
-        if(UhsGetDescriptorString(handle, profile->if_handle, string_index, 0, buffer, 512) > 0) {
-            WHBLogPrintf("%d: %s", string_index, buffer);
-        }
-    }
+void read_descriptor_string(HIDDevice *device) {
+   int32_t result;
+   size_t read_size;
+   char fmtbuffer[256];
+   char *top = &fmtbuffer[0];
+   uint8_t *read_buffer = (uint8_t *)alloc_cachealigned_zeroed(device->maxPacketSizeRx, &read_size);
+
+   result = HIDGetDescriptor(device->handle, 3, 0, 0, read_buffer, read_size, NULL, NULL);
+   for(int i = 0; i < result; i++) {
+      sprintf(top, "%02x ", read_buffer[i]);
+      top += 3;
+   }
+   WHBLogPrintf("%s\n", fmtbuffer);
+   for(int i = 1; i < 4; i++) {
+      result = HIDGetDescriptor(device->handle, 3, i, 0, read_buffer, read_size, NULL, NULL);
+      if(result > 0) {
+         WHBLogPrintf("result: %d (0x%08x)\n", result, result);
+         memset(fmtbuffer, 0, 256);
+         top = &fmtbuffer[0];
+         for(int i = 2; i < result; i += 2) {
+            top[0] = read_buffer[i];
+            top++;
+         }
+         WHBLogPrintf("descriptor string %d: %s\n", i, fmtbuffer);
+      }
+   }
+   
+   free(read_buffer);
 }
